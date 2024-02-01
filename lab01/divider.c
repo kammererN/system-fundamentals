cadf6795c77bb3592cc32568c32a3d2bd0acf458
nkam <56482517+kammererN@users.noreply.github.com>
2024-02-01 00:19:01 -0500

    Added lab01 and lab02

---
+++ lab01/divider.c
@@ -0,0 +1,26 @@
+#include  <stdio.h>
+int main() 
+{
+  
+  int number01;
+  int number02;
+  int quotient;
+  int remainder;
+
+  printf("This program finds the quotient and remainder of two numbers. \n");
+
+  printf("Enter number 01: ");
+  scanf("%i",&number01);
+
+  printf("Enter number 02: ");
+  scanf("%i",&number02);  
+
+  quotient = number01 / number02;
+  remainder = number01 % number02;
+
+  printf("===================================\n"); 
+  printf("The quotient of %i and %i is %i, with a remainder of %i.\n", number01, number02, quotient, remainder);
+
+  // Return success
+  return(0);
+}
