1fcd2d5e977aa4c0e4f8d17ef40fd36fc774fda3
Nick <56482517+kammererN@users.noreply.github.com>
2024-02-25 23:04:23 -0500

    Update lab04.c

--- lab04/lab04.c
+++ lab04/lab04.c
@@ -1,20 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Author: Nicholas Kammerer
-// Date: 2024-02-11
+// Date: 2024-02-25
 /* 
- *   ICSI 333 Lab 04. File reader.   
+ *   ICSI 333 Lab 04.
+ *   Write a program that reads a file, line by line, and prints every line on screen word by word
+ *   starting every word from new line (any sequence delimited by one or more white symbols is
+ *   considered as a word). Submit your .c file
  */
 #include <stdio.h>
 #include <string.h>
 
 const size_t LINE_SIZE = 1024; 
 const size_t FILENAME_BUFFER_SIZE = 64;
-
-/*
-Write a program that reads a file, line by line, and prints every line on screen word by word
-starting every word from new line (any sequence delimited by one or more white symbols is
-considered as a word). Submit your .c file
-*/
 
 int main(int argc, char* argv[])
 { 
